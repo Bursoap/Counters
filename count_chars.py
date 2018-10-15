3a50c1e69f162fbc2761ac1698b7518156098a55
Bursoap <andrei_kulikov@icloud.com>
2018-10-15 14:42:13 +0300

    add chars count

---
+++ count_chars.py
@@ -0,0 +1,16 @@
+class CharsCounter:
+    def __init__(self, sequence):
+        if isinstance(sequence, str):
+            self.seq = sequence
+        else:
+            raise ValueError("sequence type should be string")
+        self.count_chars()
+
+    def count_chars(self):
+        count_dict = {}
+        for char in self.seq:
+            val = count_dict.get(char, 0) + 1
+            count_dict[char] = val
+        result = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)[:3]
+        for char, quantity in result:
+            print(f"Char {char} in sequence repeated {quantity} times")
